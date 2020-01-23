484309d32a8b4814bdf2daf1bbc24c33fe556084
Kim Kyu San <developerstarlight@gmail.com>
2020-01-23 14:05:53 +0900

    update display

--- index.js
+++ index.js
@@ -20,8 +20,11 @@ $(() => {
   updateScreen()
 })
 
-function updateScreen() {
+function updateScreen(msg) {
   Object.values(window.selectors).forEach(v => {
     v.prop('disabled', !window.isOnSelection)
   })
+
+  window.shower.score.text(`${window.gameData.score.player} : ${window.gameData.score.pc}`)
+  window.shower.msg.text(msg || '아래의 버튼을 눌러 선택하세요.')
 }
